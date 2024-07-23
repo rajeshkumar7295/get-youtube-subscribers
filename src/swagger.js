9b20a816a173c865fa46036175439fa337d54fcc
rajeshkumar7295 <rajeshkumar7295949483@gmail.com>
2024-07-23 10:24:01 +0530

    add api rul

--- src/swagger.js
+++ src/swagger.js
@@ -11,7 +11,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: 'http://localhost:3000',
+      url: process.env.API_URL || 'http://localhost:3000',
       description: 'Development server',
     },
   ],
